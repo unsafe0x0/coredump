96d1a1c696f2b2dbde024ba2794e757e4d7f163b
unsafe0x0 <priyannxhuu@gmail.com>
2025-08-02 18:56:10 +0530

    fixed eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,3 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
@@ -17,8 +6,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
-
-export default eslintConfig;
